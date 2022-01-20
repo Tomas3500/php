793a21417d2bd053b146a5955f652ccc124054a3
Aleksandr <smirnov9323@gmail.com>
2022-01-20 19:25:58 +0200

    fix-2

--- src/public/form.php
+++ src/public/form.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-require './function.php';
+//session_start();
+//require './function.php';
 
 if (isset($_POST['action'])) {
     $action = trim($_POST['action']);
@@ -33,86 +33,80 @@ if (isset($_POST['action'])) {
 };
 
 
-function validFormRegistUser()
-{
-    $user = '';
-    $login = '';
-    $password = '';
-    $phone = '';
-    $count = 0;
+$user = '';
+$login = '';
+$password = '';
+$phone = '';
+$count = 0;
 
-    if (isset($_POST['user'])) {
-        $user = trim(htmlspecialchars($_POST['user']));
-        setcookie('userName', $user);
-        if ($user == '' || strlen($user) < 2) {
-            setcookie('errorUser', 'error');
-            $count++;
+if (isset($_POST['user'])) {
+    $user = trim(htmlspecialchars($_POST['user']));
+    setcookie('userName', $user);
+    if ($user == '' || strlen($user) < 2) {
+        setcookie('errorUser', 'error');
+        $count++;
 
-        } else {
-            setcookie('errorUser', '', time() - 3600);
-        }
-
-
-    }
-    if (isset($_POST['login'])) {
-        $login = trim(htmlspecialchars($_POST['login']));
-        setcookie('login', $login);
-        if ($login == '' || strlen($login) < 2) {
-            setcookie('loginError', 'error');
-            $count++;
-        } else {
-            setcookie('loginError', '', time() - 3600);
-        }
-
-    }
-    if (isset($_POST['password'])) {
-        $password = trim(htmlspecialchars($_POST['password']));
-        if ($password == '' && strlen($password) < 2) {
-            setcookie('passwordError', 'error');
-            $count++;
-        } else {
-            setcookie('passwordError', '', time() - 3600);
-        }
-
-    }
-
-    if (isset($_POST['phone'])) {
-        $phone = trim(htmlspecialchars($_POST['phone']));
-        setcookie('phone', $phone);
-        if ($phone == '' && strlen($phone) !== 10) {
-            setcookie('phoneError', 'error');
-            $count++;
-        } else {
-            setcookie('phoneError', '', time() - 3600);
-        }
-
-    }
-    if ($count) {
-        header('Location: http://example.palmo/registUser.php');
     } else {
-
-        $_SESSION['user'] = $user;
-        $_SESSION['login'] = $login;
-        $_SESSION['password'] = $password;
-        $_SESSION['phone'] = $phone;
-        header('Location: http://example.palmo/home.php');
+        setcookie('errorUser', '', time() - 3600);
     }
+
+
+}
+if (isset($_POST['login'])) {
+    $login = trim(htmlspecialchars($_POST['login']));
+    setcookie('login', $login);
+    if ($login == '' || strlen($login) < 2) {
+        setcookie('loginError', 'error');
+        $count++;
+    } else {
+        setcookie('loginError', '', time() - 3600);
+    }
+
+}
+if (isset($_POST['password'])) {
+    $password = trim(htmlspecialchars($_POST['password']));
+    if ($password == '' && strlen($password) < 2) {
+        setcookie('passwordError', 'error');
+        $count++;
+    } else {
+        setcookie('passwordError', '', time() - 3600);
+    }
+
+}
+
+if (isset($_POST['phone'])) {
+    $phone = trim(htmlspecialchars($_POST['phone']));
+    setcookie('phone', $phone);
+    if ($phone == '' && strlen($phone) !== 10) {
+        setcookie('phoneError', 'error');
+        $count++;
+    } else {
+        setcookie('phoneError', '', time() - 3600);
+    }
+
+}
+if ($count) {
+    header('Location: http://example.palmo/registUser.php');
+} else {
+    $_SESSION['user'] = $user;
+    $_SESSION['login'] = $login;
+    $_SESSION['password'] = $password;
+    $_SESSION['phone'] = $phone;
+    header('Location: http://example.palmo/home.php');
 }
 
 
 //authorization
-function authorizationUser()
-{
-    if ($_SESSION['login']) {
-        if ($_SESSION['login'] == $_POST['loginUser'] && $_SESSION['password'] == $_POST['passwordUser']) {
-            header('Location: http://example.palmo/home.php');
-        } else {
-            header('Location: http://example.palmo/login.php');
-        }
+
+if (isset($login)) {
+    echo $login;
+    if ($_POST['loginUser'] === $_SESSION['login'] && $_POST['passwordUser'] === $_SESSION['password']) {
+        header('Location: http://example.palmo/home.php');
     } else {
         header('Location: http://example.palmo/login.php');
     }
+
 }
 
-authorizationUser();
-validFormRegistUser();
+
+//validFormRegistUser();
