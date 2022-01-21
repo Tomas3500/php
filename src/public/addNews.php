88e9efa27e692e66567d6298da52005a00fa092b
Aleksandr <smirnov9323@gmail.com>
2022-01-21 20:25:05 +0200

    exem

---
+++ src/public/addNews.php
@@ -0,0 +1,95 @@
+<?php
+include 'News.php';
+$object = new  News();
+$object->validForm();
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>addNew</title>
+</head>
+<body>
+<div class="form-new">
+    <form action="addNews.php" method="post" name="addNew" enctype="multipart/form-data">
+        <p>
+            <lable>Название новости:<input type="text" name="nameNews"
+                                           value="<?php echo isset($_COOKIE['nameNews']) ? $_COOKIE['nameNews'] : '' ?>">
+            </lable>
+            <span class="error">
+                 <?php
+                 if (isset($_COOKIE['error_name'])) {
+                     echo $_COOKIE['error_name'];
+                 }
+
+                 ?>
+            </span>
+        </p>
+        <p>
+            <lable>Краткое описание новости:<input type="text" name="descNews"
+                                                   value="<?php echo isset($_COOKIE['descNews']) ? $_COOKIE['descNews'] : '' ?>">
+            </lable>
+            <span class="error">
+                 <?php
+                 if (isset($_COOKIE['error_desc'])) {
+                     echo $_COOKIE['error_desc'];
+                 }
+
+                 ?>
+            </span>
+        </p>
+        <p>
+            <lable>Дата создания новости:<input type="date" name="dataNews"</lable>
+            <span class="currentDate">
+                 <?php
+                 if (isset($_COOKIE['today'])){
+                     echo $_COOKIE['today'];
+
+                 }
+                 ?>
+            </span>
+        </p>
+        <hr>
+        <p>
+            <lable>Черновик:<input type="radio" name="radio" value="one"></lable>
+        </p>
+        <p>
+            <lable>Опубликовано:<input type="radio" name="radio" value="two"></lable>
+        </p>
+        <div class="text">
+            <p>Текст новости</p>
+            <textarea name="textDescripton" id="text" cols="100" rows="20">
+                <?php echo isset($_COOKIE['textDescripton']) ? $_COOKIE['textDescripton'] : '' ?>"
+            </textarea>
+            <span class="error">
+                   <?php
+                   if (isset($_COOKIE['error_descripton'])) {
+                       echo $_COOKIE['error_descripton'];
+                   }
+                   ?>
+            </span>
+        </div>
+        <p>
+
+            <lable>Автор новости:<input type="text[]" name="authorNews" value="<?php echo isset($_COOKIE['authorNews']) ? $_COOKIE['authorNews'] : '' ?>"></lable>
+            <?php
+            if (isset($_COOKIE['error_author'])) {
+                echo $_COOKIE['error_author'];
+            }
+            ?>
+        </p>
+        <p>
+            <lable>Загрузить фото: <input type="file" name="uploadFile"></lable>
+        </p>
+        <p>
+            <lable><input type="submit" name="btn" value="Добавить"></lable>
+        </p>
+    </form>
+</div>
+</body>
+</html>
--- src/public/index.php
+++ src/public/index.php
@@ -1,106 +1,15 @@
 <!doctype html>
-<html class="no-js" lang="">
-
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Palmo Helloe world</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <link rel="manifest" href="site.webmanifest">
-    <meta name="theme-color" content="#fafafa">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>exam</title>
 </head>
-
-<body style="margin: 0;">
-<!-- Add your site or application content here -->
-<main style="display: flex; justify-content:center; margin-top: 25px;">
-    <!--    <img src="palmo.jpg" alt="">-->
-    <br>
-    <?php
-    echo 'Hello Palmo', "<br/>";
-    $name = 'Aleksandr';
-    echo "Меня зовут ", $name, "<br/>";
-    define('pi', 3.14);
-    echo "число: pi ", pi, "<br/>";
-    $cost = '300';
-    $newCost = (integer)$cost;
-    echo $newCost * 2;
-    ?>
-</main>
-<div style="display: flex; justify-content:center; margin-top: 25px class=" hom1
-">
-<?php
-//home-1
-//task1
-$name = 'Aleksanrd';
-echo $name, "<br/>";
-//task2
-$a = 8;
-$b = 1;
-echo " " . $a + $b;
-echo " " . $a - $b;
-echo " " . $a * $b;
-echo " " . $a / $b;
-echo " " . $a % $b, "<br/>";
-//task3
-$number = 5;
-$result = $number ** 3;
-echo $result, "<br/>";
-//task4
-$age = 28;
-if ($age > 18 && $age < 60) {
-    echo "Вам ещё работать и работать", "<br/>";
-} elseif ($age < 18) {
-    echo "Вам ещё рано работать", "<br/>";
-
-} else {
-    echo 'Пора на отдых', "<br/>";
-}
-//task6
-$dayNumber = 6;
-if ($dayNumber >= 1 && $dayNumber <= 5) {
-    echo "Это рабочий день";
-
-} elseif ($dayNumber >= 6 && $dayNumber <= 7) {
-    echo "Это выходной";
-} else {
-    echo 'Ошибка';
-}
-//task7
-const DAYS_COUNT = 7;
-const MONTH_COUNT = 12;
-//define('DAYS_COUNT', 7);
-//define('MONTH_COUNT', 12);
-echo "<br/>";
-echo DAYS_COUNT, " " . MONTH_COUNT;
-//task8
-echo "<br/>";
-$numberOne = 1;
-$numberTwo = 10;
-
-if ($numberOne == $numberTwo) {
-    $resSum = $numberOne + $numberTwo;
-    echo "Сумма: " . $resSum;
-} else {
-    $resSum = $numberOne - $numberTwo;
-    echo "Разница: " . $resSum;
-}
-//task9
-echo "<br/>";
-$randomNumber = rand(1, 100);
-if ($randomNumber % 3 == 0) {
-    $a = $randomNumber;
-    echo $a;
-} else {
-    echo 'число не кратно';
-}
-?>
-</div>
-<?php
-phpinfo()
-
-?>
+<body>
+<p><a href="addNews.php">создание новостей </a></p>
+<p><a href="newsList.php">просмотр списка новостей</a></p>
 </body>
 </html>
-<?php
-//phpinfo();
